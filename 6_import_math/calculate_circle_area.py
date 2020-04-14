c648f4cb1a7fdb72c6ca1c18a6be4bbe2f623f0a
Rita Lin <mrit651529@gmail.com>
2020-04-14 16:31:15 +0800

    新增function

--- 6_import_math/calculate_circle_area.py
+++ 6_import_math/calculate_circle_area.py
@@ -6,4 +6,10 @@ r = float(input('輸入圓半徑(cm): '))
 area = math.pi * r**2
 print('圓面積: {} (cm^2)'.format(area))
 
-#123
+def area(r):
+    import math
+    area = math.pi * (r**2)
+    return area
+
+area = area (5)
+print('圓面積: ' +str(area) + '(cm^2)')
